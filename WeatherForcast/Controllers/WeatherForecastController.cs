de6f927ec62558797e6fe6cc765b7ad1a945516d
Yasteel <yasteel.gunga01@gmail.com>
2023-04-20 01:53:39 +0200

    Added other views

--- WeatherForcast/Controllers/WeatherForecastController.cs
+++ WeatherForcast/Controllers/WeatherForecastController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForcast.Models;
+using WeatherForcast.Services;
 
 namespace WeatherForcast.Controllers
 {
     public class WeatherForecastController : Controller
     {
+        private readonly CacheService cache;
+
+        public WeatherForecastController(CacheService cache)
+        {
+            this.cache = cache;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Location()
+        {
+
+            var apiData = this.cache.Get("cacheData");
+
+            if(apiData is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(apiData.Location);
+        }
+
+        public IActionResult Current()
+        {
+            var apiData = this.cache.Get("cacheData");
+
+            if (apiData is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(apiData.Current);
         }
     }
 }
--- WeatherForcast/Models/CurrentModel.cs
+++ WeatherForcast/Models/CurrentModel.cs
@@ -1,16 +1,32 @@
-﻿namespace WeatherForcast.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForcast.Models
 {
     public class CurrentModel
     {
         public string? Last_updated { get; set; }
+
+        [Display(Name = "Temp (C)")]
         public float Temp_c { get; set; }
         public ConditionModel Condition { get; set; }
+
+
+        [Display(Name = "Wind (Km/h)")]
         public float Wind_kph { get; set; }
         public int Wind_degree { get; set; }
+
+        [Display(Name = "Wind Direction")]
         public string? Wind_dir { get; set; }
+
+        [Display(Name = "Pressure")]
         public float Pressure_mb { get; set; }
+
+        [Display(Name = "Precipitation (mm)")]
         public float Precip_mm { get; set; }
+
         public int Humidity { get; set; }
+
+        [Display(Name = "Feels Like (C)")]
         public float Feelslike_c { get; set; }
     }
 }
